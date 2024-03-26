037133369d4403ea0f3c606bf1744a02b720c03d
Clinton Clark <clinton@Clintons-MacBook-Air.local>
2024-03-25 22:41:25 -0500

    stdin and scanner to echo input

--- main.go
+++ main.go
@@ -1,9 +1,15 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"os"
 )
 
 func main() {
-	fmt.Println("hello world")
+	scanner := bufio.NewScanner(os.Stdin)
+	scanner.Scan()
+	text := scanner.Text()
+
+	fmt.Println("Echoing: ", text)
 }
